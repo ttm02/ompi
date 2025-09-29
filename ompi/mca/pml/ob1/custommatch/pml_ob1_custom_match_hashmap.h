00e205412ccb3e9046fb53ae235be5c550a8713e
Tim Jammer <tim.jammer@tu-darmstadt.de>
2025-09-29 13:17:20 +0200

    implement hashfunc with only one mask

--- ompi/mca/pml/ob1/custommatch/pml_ob1_custom_match_hashmap.h
+++ ompi/mca/pml/ob1/custommatch/pml_ob1_custom_match_hashmap.h
@@ -121,14 +121,21 @@ typedef struct hashmap {
 typedef hashmap custom_match_prq;
 
 // simple hash function should suffice
-// TODO evaluate other hash functions?
 #ifndef MATCHING_HASH_FUNC_IS_PROVIDED
+#if NUM_BUCKETS %2 ==0
+static inline int matching_hash_func(int tag, int peer)
+{
+    int mask = NUM_BUCKETS-1;
+    return (tag + peer) &mask;
+}
+#else
 static inline int matching_hash_func(int tag, int peer)
 {
     int mask = 0x7FFFFFFF; // only sign bit not set
     // tag may be negative on some internal communication
     return ((tag& mask) + peer) % NUM_BUCKETS;
 }
+#endif
 #endif
 
 static inline void* to_memory_pool(hashmap *map, bucket_node *node)
